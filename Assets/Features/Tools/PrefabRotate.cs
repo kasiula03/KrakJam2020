1bf117e7a460f8e6e2dba027ca71ca96202d8da1
Szymon Zachara <szymonzachara@gmail.com>
2020-02-01 22:35:57 +0100

    snop dyskotekowego swiatla wskazujacego miejsce gdzie mozna sobie poprogramowac

---
+++ Assets/Features/Tools/PrefabRotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PrefabRotate : MonoBehaviour
+{
+    void Update()
+    {
+        float f = transform.localRotation.eulerAngles.z;
+        f += Time.deltaTime * 120f;
+        transform.localRotation = Quaternion.Euler(0f, 0f, f);
+    }
+}
---
+++ Assets/Features/Tools/RotatePalette.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePalette : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _rednerer;
+    void Update()
+    {
+        Color.RGBToHSV(_rednerer.color, out var h, out var s, out var v);
+        h += Time.deltaTime;
+        var c  = Color.HSVToRGB(h, s, v);
+        _rednerer.color = new Color( c.r, c.g, c.b, .5f);
+        
+    }
+}
