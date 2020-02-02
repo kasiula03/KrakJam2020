be6ec9fba9809736a244ec35b0ffe57d79ec60a4
Szymon Zachara <szymonzachara@gmail.com>
2020-02-02 07:31:53 +0100

    Disable colliderrs by color

---
+++ Assets/Features/ColliderByColor/ColliderByColor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+[RequireComponent(typeof(Collider2D))]
+public class ColliderByColor : MonoBehaviour
+{
+    public enum EColor
+    {
+        None,
+        Red,
+        Green,
+        Blue
+    }
+    
+    [SerializeField] private Collider2D _collider;
+    [Inject] private WorldEditorLogic _world;
+    [SerializeField] private EColor colliderColor;
+    void Reset()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+        _world.ColorResult.Subscribe(OnColorChanged).AddTo(this);
+        
+    }
+
+    void OnColorChanged(Color c)
+    {
+        if (this.colliderColor == EColor.Red && Mathf.Approximately(c.r, 0))
+        {
+            _collider.enabled = false;
+            return;
+        }
+        
+        if (this.colliderColor == EColor.Green && Mathf.Approximately(c.g, 0))
+        {
+            _collider.enabled = false;
+            return;
+        }
+        
+        if (this.colliderColor == EColor.Blue && Mathf.Approximately(c.b, 0))
+        {
+            _collider.enabled = false;
+            return;
+        }
+
+        _collider.enabled = true;
+    }
+}
---
+++ Assets/Features/InputListener/ShowInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowInput : MonoBehaviour
+{
+    public Color colorWhenInactive = new Color(1f, 1f, 1f, .7f);
+    public Color colorWhenActive = new Color(1f, 1f, 1f, 1f);
+    public Graphic output;
+    public KeyCode listenFor;
+
+    void Update()
+    {
+        if (Input.GetKey(listenFor))
+        {
+            output.color = colorWhenActive;
+        }
+        else
+        {
+            output.color = colorWhenInactive;
+        }
+    }
+}
