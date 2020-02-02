6a600878f1005cef1616d15e1bf3eb46c7e29ac2
Szymon Zachara <szymonzachara@gmail.com>
2020-02-02 08:23:20 +0100

    Cheats

---
+++ Assets/Features/Cheat/Cheat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class Cheat : MonoBehaviour
+{
+    [Inject] private PlayerAbilitiesLogic _playerAbilities; 
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            _playerAbilities.GetProperty(Abilities.BindableReason.LeftMovement).Value =
+                Abilities.BindableReaction.MoveLeft;
+            _playerAbilities.GetProperty(Abilities.BindableReason.RightMovement).Value =
+                Abilities.BindableReaction.MoveRight;
+            _playerAbilities.GetProperty(Abilities.BindableReason.FireButtonPressed).Value =
+                Abilities.BindableReaction.Fire;
+            _playerAbilities.GetProperty(Abilities.BindableReason.JumpButtonPressed).Value =
+                Abilities.BindableReaction.Jump;
+        }
+    }
+}
